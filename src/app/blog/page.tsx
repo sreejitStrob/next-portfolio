e59383886685f07d524812cee6b460b10230262f
Sreejit <sreejit9821@gmail.com>
2025-03-04 23:31:53 +0530

    minor fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -27,7 +27,6 @@ export default function Home() {
                 className="mt-10 block bg-secondary px-10 py-4 text-center font-body text-xl font-semibold text-white transition-all duration-300 hover:bg-green hover:scale-105 sm:inline-block sm:text-left sm:text-2xl"
                 href="/"
             >
-
               Say Hello!
             </Link>
         </div>
